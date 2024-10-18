5c94f383dc1048121c2514e7872d26377894d71c
seremare9 <sere6819@gmail.com>
2024-10-17 21:41:57 -0300

    saco el filtro

--- cliente.py
+++ cliente.py
@@ -10,8 +10,10 @@ dest_ip = '127.0.0.1'
 dest_port = 8000
 src_port = 5000
 
-num_seq = random.randint(1, 99)
+num_seq = random.randint(1, 10000)
 num_ack = 0
+
+print(num_seq)
 
 ip = IP(dst=dest_ip,src =source_ip)
 tcp = TCP(dport=dest_port, sport =src_port, seq=num_seq, ack=num_ack, flags="S")
@@ -19,19 +21,22 @@ syn_packet = ip/tcp
 
 respuesta = False
 
+# print("Imprimo packet")
+# syn_packet.show()
+# print("Fin packet")
+
 f.envio_paquetes_inseguro(syn_packet)
 
-'''
-while respuesta == False: 
-    pkt_capturado = sniff(iface = interface, filter=filter_str, prn=lambda x: x.show(), count=1, timeout=3)
-    if pkt_capturado:
-        respuesta = True
-    else:
-        f.envio_paquetes_inseguro(syn_packet) # Retransmito el packet
+# while respuesta == False: 
+#     pkt_capturado = sniff(iface = "lo0", prn=lambda x: x.show(), count=1, timeout=3)
+#     if pkt_capturado:
+#         respuesta = True
+#     else:
+#         f.envio_paquetes_inseguro(syn_packet) # Retransmito el packet
 
 # Examino el packet recibido y mando ACK
 
-pkt_capturado.show()
-'''
+
+
 
 
--- servidor.py
+++ servidor.py
@@ -4,41 +4,24 @@ from scapy.all import TCP, IP
 
 # print(conf.ifaces)
 
-interface = "lo0" # loopback en Mac 
+interface = "lo0" # loopback en Mac, en Windows hay que usar otra (descomentar linea 5 para ver las opciones)
 
-listen_port = 8000  # Elegir el puerto que esta escuchando
+listen_port = 8000  
 
 print(f"Listening for TCP packets on port {listen_port}...")
-filter_str = f"tcp port {listen_port}"
+# filter_str = f"tcp port {listen_port}" 
 
-tiempo = 10 # vamos probando con distintos valores
+tiempo = 30 # le puse un valor cualquiera
 
-# Escuchar en ese puerto
-pkt_capturado = sniff(iface = interface, filter=filter_str, prn=lambda x: x.show(), count=1, timeout=tiempo)
+pkt_capturado = sniff(iface = interface, prn=lambda x: x.show(), count=1, timeout=tiempo)
 
-if not pkt_capturado:
-    print("No llego nada")
+for packet in pkt_capturado:
+    print(packet.show())
 
+# if not pkt_capturado:
+#     print("No llego nada") # esto es solo para probar 
 
-# reviso el packet recibido y mando el packet SYN ACK
-# checksum = pkt_capturado.compute_chksum()
-'''
-source_ip = '127.0.0.1'
-dest_ip = '127.0.0.1'
-dest_port = 5000 #puerto del cliente
-src_port = 8000 #puerto del server
+# print("IMPRIMO PACKET CAPTURADO")
+# pkt_capturado.show()
+# print("FIN PACKET CAPTURADO")
 
-num_seq = 0
-
-num_ack = 0
-
-ip = IP(dst=dest_ip,src =source_ip)
-
-tcp = TCP(dport=dest_port, sport =src_port, seq=num_seq, ack=num_ack, flags="S")
-
-syn_ack_packet = ip/tcp
-
-f.envio_paquetes_inseguro(syn_ack_packet)
-
-# Aca tengo que implementar el mismo sistema de retransmisiones que el cliente
-'''
