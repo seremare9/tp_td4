4b7cf6da8bc1ab2967b97248d624c750a15c5ec2
Clarita Giralt Font <claritagiraltfont@gmail.com>
2024-10-30 21:34:41 -0300

    cambios llamada

--- cliente.py
+++ cliente.py
@@ -5,119 +5,134 @@ import time
 import random
 from funcionesAuxiliares import *
 
-retransmisiones_finack = 0 # una herramienta misteriosa que nos ayudará más tarde
-
 # Elegimos parametros
+global source_ip
+global dest_ip
+global dest_port
+global src_port
+
 source_ip = '127.0.0.1'
 dest_ip = '127.0.0.1'
 dest_port = 8000
 src_port = 5000
 
-num_seq = random.randint(1, 10000) 
-num_ack = 0
-
-print(num_seq)
-
-# Armamos el paquete
-ip = IP(dst=dest_ip,src =source_ip)
-tcp = TCP(dport=dest_port, sport =src_port, seq=num_seq, ack=num_ack, flags="S")
-syn_packet = ip/tcp 
-
-ultimo_packet_enviado = "SYN" # Variable para tener registro de las flags enviadas por el cliente.
-
-f.envio_paquetes_inseguro(syn_packet) # Se envía el paquete que contiene el SYN.
-
 interface = "Software Loopback Interface 1" 
 # interface = "lo0"
 
-conectado = True 
+def handshake_cliente():
+    retransmisiones_finack = 0 # una herramienta misteriosa que nos ayudará más tarde
+    # Asignamos nro de secuencia y de ACK
+    ultimo_packet_enviado = "NONE"
+    if ultimo_packet_enviado == "NONE":
+        num_seq = random.randint(1, 10000) 
+        num_ack = 0
+        print(num_seq)
 
-print(f"Listening for TCP packets on port {src_port}...")
-filter_str = f"tcp port {src_port}"
+        # Armamos el paquete
+        ip = IP(dst=dest_ip,src =source_ip)
+        tcp = TCP(dport=dest_port, sport =src_port, seq=num_seq, ack=num_ack, flags="S")
+        syn_packet = ip/tcp 
 
-while conectado: # Acá manejamos todo lo que pasa después de que se envia el SYN
+        ultimo_packet_enviado = "SYN" # Variable para tener registro de las flags enviadas por el cliente.
+
+        f.envio_paquetes_inseguro(syn_packet) # Se envía el paquete que contiene el SYN.
+
+
+    conectado = True 
 
     print(f"Listening for TCP packets on port {src_port}...")
     filter_str = f"tcp port {src_port}"
 
-    pkt_capturado = sniff(iface = interface, filter=filter_str, count=1, timeout=3) 
+    while conectado: # Acá manejamos todo lo que pasa después de que se envia el SYN
 
-    if pkt_capturado: # Si capturó un paquete sin delay
+        print(f"Listening for TCP packets on port {src_port}...")
+        filter_str = f"tcp port {src_port}"
 
-        print(pkt_capturado)
+        pkt_capturado = sniff(iface = interface, filter=filter_str, count=1, timeout=3) 
 
-        paquete = pkt_capturado[0]
+        if pkt_capturado: # Si capturó un paquete sin delay
 
-        flag = paquete[TCP].flags
+            print(pkt_capturado)
 
-        flags_esperadas = ("SA", "A", "F")
+            paquete = pkt_capturado[0]
 
-        if not(flag in flags_esperadas):
-            continue # Paso a la siguiente iteración
+            flag = paquete[TCP].flags
 
-        pkt_capturado[0].show()
+            flags_esperadas = ("SA", "A", "F")
 
-        # Checksum
-        
-        # ip_checksum = paquete[IP].chksum
-        tcp_checksum = paquete[TCP].chksum
+            if not(flag in flags_esperadas):
+                continue # Paso a la siguiente iteración
 
-        # print(f"Checksum IP del paquete con la flag {flag}: {ip_checksum}")
-        print(f"Checksum TCP del paquete con la flag {flag}: {tcp_checksum}")
+            pkt_capturado[0].show()
 
-        paquete[TCP].chksum = 0
-        ph = pseudo_header(paquete[IP].src, paquete[IP].dst, paquete[IP].proto, len(paquete[TCP]))
-        checksum_calculado = checksum(bytes(paquete[TCP]) + ph)
-        print(checksum_calculado)
-
-        if tcp_checksum != checksum_calculado:
-            print(f"El paquete con la flag {flag} está corrupto")
-            continue # Sigue escuchando
-        
-
-        if flag == "SA": # Si recibe un SYN+ACK, manda un ACK
-            ip = IP(dst=dest_ip,src =source_ip)
-            tcp = TCP(dport=dest_port, sport =src_port, seq=paquete[TCP].ack, ack=paquete[TCP].seq+1, flags="A")
-            ack_packet = ip/tcp 
-            ultimo_packet_enviado = "ACK"
-            f.envio_paquetes_inseguro(ack_packet) # Se envía el paquete que contiene el ACK
-            time.sleep(20) # Espera los 20 segundos que se toma el servidor en mandar el FIN
-
-        elif flag == "F":
-            ip = IP(dst=dest_ip,src =source_ip)
-            tcp = TCP(dport=dest_port, sport =src_port, seq=paquete[TCP].ack, ack=paquete[TCP].seq+1, flags="FA")
-            finack_packet = ip/tcp 
-            ultimo_packet_enviado = "FIN_ACK"
-            f.envio_paquetes_inseguro(finack_packet) # Se envía el paquete que contiene el FIN+ACK 
-
-        elif flag == "A": # Recibe el último ACK. Ya se puede cerrar la conexión
-            conectado = False
-            
-        else:
-            continue 
+            if not check_sum(paquete):
+                print(f"El paquete con la flag {flag} está corrupto")
+                continue # Sigue escuchando
                 
-    else: # Si pasaron 3 segundos y no recibí ningún paquete
-        
-        if ultimo_packet_enviado == "SYN": # Retransmito el paquete SYN
-            f.envio_paquetes_inseguro(syn_packet)
-            
-        elif ultimo_packet_enviado == "ACK": # Retransmito el paquete ACK
-            f.envio_paquetes_inseguro(ack_packet)
 
-        elif ultimo_packet_enviado == "FIN_ACK":
-            '''
-            El cliente retransmitirá el FIN ACK varias veces. Si después de un número determinado de retransmisiones 
-            aún no recibe el ACK, el cliente terminará cerrando la conexión de manera unilateral, agotando su temporizador.
-            '''
-            if retransmisiones_finack < 5: # Se asume que el servidor cerró la conexión
-                f.envio_paquetes_inseguro(finack_packet)
-                retransmisiones_finack += 1
+            if flag == "SA": # Si recibe un SYN+ACK, manda un ACK
+                ip = IP(dst=dest_ip,src =source_ip)
+                tcp = TCP(dport=dest_port, sport =src_port, seq=paquete[TCP].ack, ack=paquete[TCP].seq+1, flags="A")
+                ack_packet = ip/tcp 
+                ultimo_packet_enviado = "ACK"
+                f.envio_paquetes_inseguro(ack_packet) # Se envía el paquete que contiene el ACK
+                time.sleep(20) # Espera los 20 segundos que se toma el servidor en mandar el FIN
+
+            elif flag == "F":
+                ip = IP(dst=dest_ip,src =source_ip)
+                tcp = TCP(dport=dest_port, sport =src_port, seq=paquete[TCP].ack, ack=paquete[TCP].seq+1, flags="FA")
+                finack_packet = ip/tcp 
+                ultimo_packet_enviado = "FIN_ACK"
+                f.envio_paquetes_inseguro(finack_packet) # Se envía el paquete que contiene el FIN+ACK 
+
+            elif flag == "A": # Recibe el último ACK. Ya se puede cerrar la conexión
+                    conectado = False
+                    
             else:
-                conectado = False # Cierro la conexión
+                continue 
+                    
+        else: # Si pasaron 3 segundos y no recibí ningún paquete
+            
+            if ultimo_packet_enviado == "SYN": # Retransmito el paquete SYN
+                f.envio_paquetes_inseguro(syn_packet)
                 
-        else: # Paso a la siguiente iteración
-            continue
+            elif ultimo_packet_enviado == "ACK": # Retransmito el paquete ACK
+                f.envio_paquetes_inseguro(ack_packet)
 
-        
-    
-print("Fin de la conexión")
+            elif ultimo_packet_enviado == "FIN_ACK":
+                '''
+                El cliente retransmitirá el FIN ACK varias veces. Si después de un número determinado de retransmisiones 
+                aún no recibe el ACK, el cliente terminará cerrando la conexión de manera unilateral, agotando su temporizador.
+                '''
+                if retransmisiones_finack < 5: # Se asume que el servidor cerró la conexión
+                    f.envio_paquetes_inseguro(finack_packet)
+                    retransmisiones_finack += 1
+                else:
+                    conectado = False # Cierro la conexión
+                    
+            else: # Paso a la siguiente iteración
+                continue
+
+    print("Fin de la conexión")
+
+
+def pseudo_header(ip_src, ip_dst, ip_proto, length):
+    """
+    Return a pseudo header according to RFC768
+    """
+    # Prepare the binary representation of the pseudo header
+    return struct.pack("!4s4sHH", inet_aton(ip_src), inet_aton(ip_dst), ip_proto, length)
+
+def check_sum(paquete) -> bool:
+    # Guardamos el valor del header en una variable
+    tcp_checksum = paquete[TCP].chksum
+    # Seteamos el valor del header en cero para recalcularlo
+    paquete[TCP].chksum = 0
+    # Creamos un pseudo header
+    ph = pseudo_header(paquete[IP].src, paquete[IP].dst, paquete[IP].proto, len(paquete[TCP]))
+    # Calculamos el checksum del contenido del paquete con el pseudo header
+    checksum_calculado = checksum(bytes(paquete[TCP]) + ph)
+
+    return tcp_checksum == checksum_calculado
+
+handshake_cliente()
